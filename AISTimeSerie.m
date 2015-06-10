5d6a810f4ceff80e27240f98b4decadf02f8097d
Vasilis-Thanos Anagnostopoulos <anagnwstopoulos@hotmail.com>
2015-06-10 13:58:39 +0300

    Adding some more commets

--- AISTimeSerie.m
+++ AISTimeSerie.m
@@ -1,5 +1,6 @@
 function [ output_args ] = AISTimeSerie( dataset, training_percentage, ...
-    population_percentage, max_iterations, r_threshold )
+    population_percentage, error_threshold, c_param ,b_param, d_param, ...
+    e_param, max_iterations )
 %AISTimeSerie This is a function for forecasting time series using an
 %artificial immune system
 %   The function is implemented using the algorithm presented in [2].
@@ -42,12 +43,38 @@ function [ output_args ] = AISTimeSerie( dataset, training_percentage, ...
 %   Series with Multiple Seasonal Cycles." Transactions on Computational 
 %   Collective Intelligence XI. Springer Berlin Heidelberg, 2013. 176-197.
 
+%   The variable training_percentage declares the percentage of the initilial
+%   population that will be used for training the AIS
+%   The population_percentage variable declares the percentage for the division
+%   of the training population into two subsets
+%   The error_threshold variable declares the error threshold value used to
+%   to assume that an antigonen y is similiar to antigon x
+%   The variable c_param declares parameter c in [0,1) that allows to adjust
+%   the cross-reactivity threshold value from rkmin = d(pk, xa) to
+%   rkmax = d(pk,xb)
+%   The variable b_param declares parameter b that plays the same role as the
+%   parameter c.
+%   The d_param variable declares parameter dy, where dy is the threshold
+%   value of the assumption that ith AGy is similiar to the kth ABy, and
+%   ith AGx, paired with this AGy, is classified to class 1 (high similiarity)
+%   else it is classified to class 2(low similiarity)
+%   The e_param variable declares parameter ex, where ex is the threshold
+%   value of the forecast error of the time series elements encoded in the
+%   paratope of the kth ABx. similiarity
+%   ΤΑ d_param και το e_param ΤΑ ΧΡΕΙΑΖΟΜΑΣΤΕ ΣΤΟΝ ΚΩΔΙΚΑ ????
+                                                  
+
+% AYTΗ Η ΜΕΘΟΔΟΣ ΔΕΝ ΕΧΕΙ ΠΟΥΘΕΝΑ ΛΟΥΠΑ ;;;
+
+% ΤΙ ΜΟΡΦΗ ΘΑ ΕΧΟΥΝ ΤΑ ΔΕΔΟΜΕΝΑ ΕΙΣΟΔΟΥ??? ΘΑ ΠΡΕΠΕΙ ΝΑ ΣΠΑΤΑΛΗΣΩ 2 ΜΕΤΑΤΒΛΗΤΕΣ
+% Η ΔΕΝ ΜΑΣ ΝΟΙΑΖΕΙ... ΠΡΟΤΙΜΩ ΜΙΑ ΜΕ 2 ΣΤΗΛΕΣ.
+
 switch nargin
     case 1
         training_percentage = 2/3; 
         population_percentage = 0.5;
+        error_threshold =
         max_iterations = 50;
-        r_threshold = 10;
     otherwise
         error ('Too few or too many arguments were entered');
 end
@@ -122,10 +149,100 @@ paratope_y = [AGy_train, ABy_train];
 % The measure of similarity of the ith AGx to the kth ABx is an affinity and 
 % the formulas are in AIS_STLF08.pdf[2]
 % Affinity informs about the degree of membership of the ith AGx to the
-% cluster represented by the kth ABx.
+% cluster represented by the kth ABx and similiarity of the ith AGy to the
+% kth ABY is measured using the forecast error of the time series elements
+% encoded in the paratope of the kth ABy. These elements are forecasted
+% using the epitope of the ith AGy [2]
 
 similiarity_AGx_to_ABx = zeros(size(AGx_train),size(ABx_train));
 
+for  index_i = 1:size(AGx_train)
+    for index_j = 1: size (ABx_train)
+        rk = d(pk,xa) + c_param(d(pk,xb)-d(pk,xa) )
+
+        % ΠΩΣ ΒΡΙΣΚΟΥΜΕ ΤΟ ΚΟΝΤΙΝΟΤΕΡΟ ΑΝΤΙΓΟΝΟ ΚΑΙ ΤΟ ΜΑΚΡΥΤΕΡΟ. ΚΑΙ ΤΙ ΕΝΝΟΥΜΕ
+        % ΚΟΝΤΙΝΟΤΕΡΟ ΜΑΚΡΥΤΕΡΟ ???? ΑΚΟΜΑ ΤΙ ΕΝΝΟΕΙ ΜΕ ΤΙΣ ΚΛΑΣΙΕΣ ΟΕΟ???
+        % ΑΝ ΘΑ ΓΙΝΕΤΑΙ ΚΑΠΟΥ ΛΟΥΠΑ ΝΑ ΦΑΝΤΑΣΤΩ ΕΙΝΑΙ ΕΔΩ ;;;
+
+        if( d> rk OR rk = 0)
+
+        else
+
+        end
+    end
+end
+
+similiarity_AGy_to_ABy = zeros(size(AGy_train),size(ABy_train));
+
+for  index_i = 1:size(AGx_train)
+    for index_j = 1: size (ABx_train)
+        % Εδώ δεν τον πιάνω καθόλου τον τύπο
+    end
+end
+
+
+% Calculation of the cross-reactivity thresholds of y-antibodies. The
+% recognition region of the kth ABx should be as large as possible and
+% cover only the AGx that satisfies two conditions:
+% (i)  their epitops y are similar to the paratope q_k, and
+% (ii) the AGx paired with them have epitopes x similar to the kth ABx
+%      paratope p_k
+% The cross-reactivity threshold of the kth ABy is calulated similarly
+% to the above and the only that chagnes is the parameter c, where
+% it is a similiar parameter b.
+                                                  
+% KATI DEN PIANW. GIATI TA YPOLOGIZOYME 2 FORES ME DIAFORETIKOUS TYPOUS???
+                                                  
+similiarity_ABx_to_AGx = zeros(size(AGx_train),size(ABx_train));
+
+for  index_i = 1:size(AGx_train)
+    for index_j = 1: size (ABx_train)
+        sk = d(pk,xa) + b_param(d(pk,xb)-d(pk,xa) )
+
+        % ΠΩΣ ΒΡΙΣΚΟΥΜΕ ΤΟ ΚΟΝΤΙΝΟΤΕΡΟ ΑΝΤΙΓΟΝΟ ΚΑΙ ΤΟ ΜΑΚΡΥΤΕΡΟ. ΚΑΙ ΤΙ ΕΝΝΟΥΜΕ
+        % ΚΟΝΤΙΝΟΤΕΡΟ ΜΑΚΡΥΤΕΡΟ ???? ΑΚΟΜΑ ΤΙ ΕΝΝΟΕΙ ΜΕ ΤΙΣ ΚΛΑΣΙΕΣ ΟΕΟ???
+        % ΑΝ ΘΑ ΓΙΝΕΤΑΙ ΚΑΠΟΥ ΛΟΥΠΑ ΝΑ ΦΑΝΤΑΣΤΩ ΕΙΝΑΙ ΕΔΩ ;;;
+
+        if( d> sk OR rk = 0)
+
+        else
+
+        end
+    end
+end
+
+similiarity_AGy_to_ABy = zeros(size(AGy_train),size(ABy_train));
+
+for  index_i = 1:size(AGx_train)
+    for index_j = 1: size (ABx_train)
+        % Εδώ δεν τον πιάνω καθόλου τον τύπο
+    end
+end
+
+
+
+% Calculation of the empirical conditional probabilities P(ABy_k | ABx_j)
+
+% TI KANOYME EDW OEO???
+% XRHSIMOPOIOUME TA d_param kai ta e_parama GIA NA KATALABOUME AN EXOUME 
+% ANTISTOIXIA KAI META TI ???. DEN TO PIANW ME TIS PITHANOTHTES!!!
+
+
+
+% Test
+% Forecast determination using y-antibodies, probalities P(ABy_k | ABx_j)
+% and affinities
+% After learning the antibodies rpresent overlapping clusters of similar
+% antigens. In the forecast procedure new antigen having only x-chain is
+% presented. The \Omega set of antibodies, activated by this antigen, is
+% determimed. The y-chains of these antibodies storage average y-chains of
+% antigens from the training set with simlar x-chains [1]. The formulas are in 
+% AIS_STLF08.pdf
+
+
+
+
+% ΜΟΥ ΠΕΡΙΣΣΕΨΑΝ … 
 % Activated antibody detection and evaluation
 % If the affinity of the antibody for the antigen is smaller than or equal
 % to the cross-reactivity threshold r, it means that the antigen lies in
@@ -174,22 +291,6 @@ similiarity_AGx_to_ABx = zeros(size(AGx_train),size(ABx_train));
     % several antigens (it depends on the value of the cross-reactivity
     % threshold r). Outlier, i.e. antigen lying away from other antigens,
     % is represented by the seperate antibody [1].
-
-
-% Calculation of the cross-reactivity thresholds of y-antibodies
-
-% Calculation of the empirical conditional probabilities P(ABy_k | ABx_j)
-
-
-% Test
-% Forecast determination using y-antibodies, probalities P(ABy_k | ABx_j)
-% and affinities
-% After learning the antibodies rpresent overlapping clusters of similar
-% antigens. In the forecast procedure new antigen having only x-chain is
-% presented. The \Omega set of antibodies, activated by this antigen, is
-% determimed. The y-chains of these antibodies storage average y-chains of
-% antigens from the training set with simlar x-chains [1]. The formulas are in 
-% AIS_STLF08.pdf
 
 end
 
